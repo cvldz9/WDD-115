4222c5cfdbbfa69512c716f1abe3c192775ab8c6
Tiger <m7mdo_o@yahoo.com>
2023-11-09 03:50:20 -0500

    Add Functionality to Navigation Dots to Change Slides on Hover while Keeping track of the Active Dot

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-/*----------TRENDING BOOKS----------*/
-var trendingSlider = new Swiper(".trending-slider", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2.5,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// Hero Slider
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
+
+// Update the active dot when change slides
+function updateActiveDot() {
+  // Remove "active" class from all dots
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  // Add "active" class to the dot corresponding to the current slide
+  dots[currentSlide].classList.add("active");
+}
 
 function showSlide(slideIndex) {
   if (slideIndex < 0) {
     currentSlide = slides.length - 1;
   } else if (slideIndex >= slides.length) {
     currentSlide = 0;
+  } else {
+    currentSlide = slideIndex;
   }
 
   for (let i = 0; i < slides.length; i++) {
@@ -37,22 +26,21 @@ function showSlide(slideIndex) {
   }
 
   slides[currentSlide].style.display = "block";
+
+  updateActiveDot();
 }
 
 function nextSlide() {
   currentSlide++;
   showSlide(currentSlide);
+  updateActiveDot();
 }
 
 function previousSlide() {
   currentSlide--;
   showSlide(currentSlide);
+  updateActiveDot();
 }
-
-// Automatically advance to the next slide every 3 seconds.
-setInterval(nextSlide, 3500);
-
-showSlide(currentSlide);
 
 // Slide Arrows
 const leftArrow = document.getElementById("leftArrow");
@@ -66,4 +54,15 @@ rightArrow.addEventListener("click", () => {
   nextSlide();
 });
 
-// End Hero Slider
+// Automatically advance to the next slide every 3 seconds.
+setInterval(nextSlide, 3500);
+
+// Select all navigation dots
+const dots = document.querySelectorAll(".dot");
+
+dots.forEach((dot) => {
+  dot.addEventListener("mouseover", (e) => {
+    const slideIndex = parseInt(e.target.getAttribute("data-slide-index"));
+    showSlide(slideIndex);
+  });
+});
