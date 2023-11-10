5f0a96d607eec61be70d4cb99a507aa8644b9a0a
Catherine Valdez <catvaldez519@gmail.com>
2023-11-09 20:47:42 -0500

    Added Free Download button + js file

---
+++ js/script2.js
@@ -0,0 +1,3 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Your free copy of the audiobook has been successfully downloaded! Please check your Downloads folder to locate the file.');
+});
